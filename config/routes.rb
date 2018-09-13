5acb0577141c0c476e3195d6538337ca89daf75d
taha.fayyaz <taha.fayyaz@thedevden.co>
2018-09-13 12:42:01 +0500

    Add home page and set as authenticated route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :employees
+  get 'pages/home'
+  devise_for :employee
+
+  devise_scope :employee do
+    authenticated :employee do
+      root  'pages#home', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
