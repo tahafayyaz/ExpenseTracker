7a005efce4033558abb6dbfc0c64358e47b5f345
taha.fayyaz <taha.fayyaz@thedevden.co>
2018-09-13 18:43:44 +0500

    Add Home logins + route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/home'
   devise_for :employee
+  root to: "pages#home"
 
-  devise_scope :employee do
-    authenticated :employee do
-      root  'pages#home', as: :authenticated_root
-    end
-
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
 end
