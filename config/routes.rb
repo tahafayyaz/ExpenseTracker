609c47f75ed8c4d7969f96b848129d51978d3960
taha.fayyaz <taha.fayyaz@thedevden.co>
2018-09-18 17:22:30 +0500

    Add employee/manager default homepage + route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :employees, controllers: { registrations: 'employees/registrations' }
-  devise_for :managers
+  devise_for :managers, controllers: { registrations: 'managers/registrations' }
   get 'pages/home'
   root to: "pages#home"
 end
