3aa3b97e47cadc32161a71ac33be501d87c2e2da
taha.fayyaz <taha.fayyaz@thedevden.co>
2018-09-18 16:30:27 +0500

    Add manager devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :employees, controllers: { registrations: 'employees/registrations' }
+  devise_for :managers
   get 'pages/home'
   root to: "pages#home"
 end
