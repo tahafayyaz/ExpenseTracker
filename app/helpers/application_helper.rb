63069ff1b40d819a388e816401e4be1f50341db7
taha.fayyaz <taha.fayyaz@thedevden.co>
2018-09-24 12:40:32 +0500

    Render image in employee&manager edit form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def render_image_tag(image)
+    image_tag(image) if image.attached?
+  end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,4 @@
 class Employee < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  has_one_attached :image
 end
